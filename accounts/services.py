1eac82670f6655a4595a8f55ac77f7442f690028
yugasaimanikanta <2200031887@kluniversity.in>
2026-06-24 20:11:45 +0530

    updated2

--- accounts/services.py
+++ accounts/services.py
@@ -1,36 +1,16 @@
 from django.conf import settings
 from django.core.mail import send_mail
+
 from .models import EmailOTP
-import threading
-
-
-def _send_email(email: str, otp: str):
-    try:
-        print("Sending OTP email to:", email)
-
-        send_mail(
-            subject="Deepam OTP",
-            message=f"OTP is {otp}",
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[email],
-            fail_silently=False,
-        )
-
-        print("Email sent successfully")
-
-    except Exception as e:
-        print("Email error:", str(e))
 
 
 def send_login_otp(email: str) -> str:
-    otp_record = EmailOTP.create_for_email(email)
-
-    # safer thread handling for Render
-    t = threading.Thread(
-        target=_send_email,
-        args=(email, otp_record.otp),
-        daemon=True   # IMPORTANT FIX
-    )
-    t.start()
-
-    return otp_record.otp
+	otp_record = EmailOTP.create_for_email(email)
+	send_mail(
+		"Deepam OTP",
+		f"OTP is {otp_record.otp}",
+		settings.EMAIL_HOST_USER,
+		[email],
+		fail_silently=False,
+	)
+	return otp_record.otp
