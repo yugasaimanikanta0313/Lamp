80d1b6b60bb81f0e2cb0615c387bf1e489579768
yugasaimanikanta <2200031887@kluniversity.in>
2026-06-24 20:31:34 +0530

    updated2

--- deepam_project/settings.py
+++ deepam_project/settings.py
@@ -121,7 +121,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ----------------------------
 # EMAIL
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND ="django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
